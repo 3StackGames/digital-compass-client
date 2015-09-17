170333d6fd7907dfbb02357305f379537ce64f7c
Jung Oh <Jung3o@yahoo.com>
2015-09-16 19:42:28 -0500

    made it so the url isnt ugly anymore

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -4,5 +4,6 @@ import 'es6-shim';
 import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
-React.render(<Router>{routes}</Router>, document.getElementById('root'));
+React.render(<Router history={createBrowserHistory()}>{routes}</Router>, document.getElementById('root'));
